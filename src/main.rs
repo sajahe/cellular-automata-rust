2fe18bb4ef63f9b006215605ff7a4cf8b7f50c64
Sampo Pietikäinen <sampo.pietikainen@gmail.com>
2020-09-20 20:12:04 +0300

    Main refactored

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 extern crate sdl2;
-
-extern crate image;
 extern crate bit_array;
-
-
+extern crate image;
 extern crate typenum;
 
+use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
-use sdl2::keyboard::Keycode;
 extern crate rand;
 
 use std::env;
@@ -19,8 +16,9 @@ mod game_of_life;
 mod setup;
 use self::rand::prelude::*;
 
-
 const CELL_SIZE: u32 = 2;
+const HELP: &str = "cellular_automata COMMAND";
+
 /**
 * These are the console command rules that should be implented
 * modes
@@ -32,135 +30,135 @@ const CELL_SIZE: u32 = 2;
 *
 */
 fn main() {
-    //let mut matrix2: [[bool; 16];8];
-    //Here the rule should be get. If no rule than some default rule.
     let args: Vec<String> = env::args().collect();
-    let life_rule = setup::build_life_rule([false,false,false,true,false,false,true,false,false], [false,true,true,false,false,true,false,false,false]);
-    //let life_rule = setup::build_life_rule([false,false,true,true,false,false,false,false,false], [false,false,false,true,true,false,false,false,false]);
-    //let life_rule = setup::build_life_rule([false,false,true,false,true,false,false,false,false], [false,false,false,false,true,false,false,false,false]);
-    //let life_rule = setup::build_life_rule([false,false,true,false,false,false,false,false,false], [false,false,false,false,false,false,false,false,false]);
-    //let life_rulegraphics = ::setup::build_life_rule([false,false,false,true,false,false,false,false,false],
-    //                                        [false,false,true,true,false,false,false,false,false]);
-    //assert_eq!(true, check_rule(&[true,false,false],30));
-    check_arguments(&args);
+    let mut query = &String::from("");
+    let mut rule = &String::from("");
     
 
-    let query = &args[1];
+    //Here the rule should be get. If no rule than some default rule.
+    match args.len() {
+        1 => {
+            println!("No second argument!");
 
-    let rule = &args[2];
+        },
+        2 => {
+            query = &args[1];
+            
+        },
+        3=> {
+            query = &args[1];
+            rule = &args[2];
+        },
+
+        _ => println!("Unknown COMMAND {}\n Help: {}", query, HELP),
+    }
+
     println!("Using {} {}", query, rule);
-    if query == "rule"{
+
+    match query.as_str() {
+        "rule" => create_image(&rule),
+        "graphics" => create_game_of_life_graphics(),
+        _ => println!("Unknown COMMAND {}\n Help: {}", query, HELP),
+    }
+}
+
+
+fn create_random_generation_vector(gen: &mut Vec<game_of_life::CellCoordinates>, amount: u32) {
+    gen.clear();
+    for _ in 0..amount {
+        gen.push(game_of_life::build_cell_coordinates(
+            rand::thread_rng().gen_range(200, 290) as u32,
+            rand::thread_rng().gen_range(190, 280) as u32,
+            true,
+        ));
+    }
+}
+
+fn create_image(rule: &String) {
     match rule.parse::<u8>() {
         Ok(n) => elementary::create_image(n),
-        Err(e) => println!("Not a number {}",e),
+        Err(e) => println!("Not a number {}", e),
     }
-    }
+}
 
-    let rule = 30;
+fn create_game_of_life_graphics() {
+    println!("Game of life");
+    let life_rule = setup::build_life_rule(
+        [false, false, false, true, false, false, true, false, false],
+        [false, true, true, false, false, true, false, false, false],
+    );
+    let mut first_gen: Vec<game_of_life::CellCoordinates> = Vec::new();
+    create_random_generation_vector(&mut first_gen, 500);
 
-
-    if query == "graphics" {
-        let mut first_gen: Vec<game_of_life::CellCoordinates> = Vec::new();
-        //let mut first_gen: HashMap<game_of_life::Coordinates, bool> = HashMap::new();
-        if rule == 24 {
-        /*
-        first_gen.insert(game_of_life::Coordinates{x:10,y:11},true);
-        first_gen.insert(game_of_life::Coordinates{x:10,y:12},true);
-        first_gen.insert(game_of_life::Coordinates{x:10,y:13},true);
-        first_gen.insert(game_of_life::Coordinates{x:18,y:23},true);
-        first_gen.insert(game_of_life::Coordinates{x:19,y:24},true);
-        first_gen.insert(game_of_life::Coordinates{x:20,y:22},true);
-        first_gen.insert(game_of_life::Coordinates{x:20,y:23},true);
-        first_gen.insert(game_of_life::Coordinates{x:20,y:24},true);
-        */
-        } else {
-            println!("Game of life");
-            //create_random_generation(&mut first_gen);
-            create_random_generation_vector(&mut first_gen, 500);
-            //return;
-        }
-
-        //let generation= game_of_life::create_next_generation(first_gen);
     let _sdl = sdl2::init().unwrap();
     let video_subsystem = _sdl.video().unwrap();
     let _window = video_subsystem
-        .window("Game of Life",900,700)
+        .window("Game of Life", 900, 700)
         .resizable()
         .build()
         .unwrap();
-        let mut canvas  =  _window.into_canvas().build().unwrap();
+    let mut canvas = _window.into_canvas().build().unwrap();
 
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+
+    canvas.clear();
+    for cell in &first_gen {
+        if cell.alive {
+            canvas.set_draw_color(Color::RGB(0, 255, 0));
+        } else {
+            canvas.set_draw_color(Color::RGB(30, 30, 30));
+        }
+        canvas
+            .fill_rect(Rect::new(
+                2 * cell.x as i32,
+                2 * cell.y as i32,
+                CELL_SIZE,
+                CELL_SIZE,
+            ))
+            .unwrap();
+    }
+
+    let mut generation = game_of_life::create_next_generation_concurrent(&first_gen, &life_rule);
+
+    canvas.present();
+
+    let mut event_pump = _sdl.event_pump().unwrap();
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit { .. } => break 'main,
+                sdl2::event::Event::KeyDown {
+                    keycode: Some(Keycode::Return),
+                    ..
+                } => {
+                    println!("Reset");
+                    create_random_generation_vector(&mut generation, 500)
+                }
+                _ => {}
+            }
+        }
+        thread::sleep(Duration::new(0, 1_000_000_000u32 / 20));
         canvas.set_draw_color(Color::RGB(0, 0, 0));
-
         canvas.clear();
-        for cell in  &first_gen {
-            println!("Game of life");
+        for cell in &generation {
             if cell.alive {
                 canvas.set_draw_color(Color::RGB(0, 255, 0));
             } else {
-                canvas.set_draw_color(Color::RGB(30, 30,30));
+                canvas.set_draw_color(Color::RGB(30, 30, 30));
             }
-            canvas.fill_rect(Rect::new(2*cell.x as i32, 2*cell.y as i32, CELL_SIZE, CELL_SIZE)).unwrap();
-            
+            canvas
+                .fill_rect(Rect::new(
+                    2 * cell.x as i32,
+                    2 * cell.y as i32,
+                    CELL_SIZE,
+                    CELL_SIZE,
+                ))
+                .unwrap();
         }
-
-        let mut generation= game_of_life::create_next_generation_concurrent(&first_gen, &life_rule);
 
         canvas.present();
-
-        let mut event_pump = _sdl.event_pump().unwrap();
-            'main: loop {
-
-                for event in event_pump.poll_iter() {
-                    match event {
-                        sdl2::event::Event::Quit {..} => break 'main,
-                        sdl2::event::Event::KeyDown { keycode: Some(Keycode::Return), .. } => {
-                            println!("Reset");
-                            create_random_generation_vector(&mut generation,500)
-
-                },
-                        _ => {},
-                    }
-                }
-                thread::sleep(Duration::new(0, 1_000_000_000u32 / 20));
-                canvas.set_draw_color(Color::RGB(0, 0, 0));
-                canvas.clear();
-                for cell in  &generation {
-                    if cell.alive {
-                        canvas.set_draw_color(Color::RGB(0, 255, 0));
-                    } else {
-                        canvas.set_draw_color(Color::RGB(30, 30,30));
-                    }
-                    canvas.fill_rect(Rect::new(2*cell.x as i32, 2*cell.y as i32, CELL_SIZE, CELL_SIZE)).unwrap();
-                }
-
-
-
-                canvas.present();
-
-
-                generation = game_of_life::create_next_generation_concurrent(&generation, &life_rule);
-
-            }
-        }
-
-}
-
-fn check_arguments(args:&Vec<String>){
-    match args[1].as_ref() {
-        "elementary" => println!("Elementary"),
-        "gol" => println!("Game of life also"),
-        "game" => println!("Game of life"),
-        _ => println!("Unknown COMMAND {}\n Usage: cellular_automata COMMAND",args[1])
+        generation = game_of_life::create_next_generation_concurrent(&generation, &life_rule);
     }
-}
-
-fn create_random_generation_vector(gen:&mut Vec<game_of_life::CellCoordinates>, amount:u32) {
-    gen.clear();
-    for _ in 0..amount{
-        gen.push(game_of_life::build_cell_coordinates(rand::thread_rng().gen_range(200, 290) as u32,rand::thread_rng().gen_range(190, 280) as u32,true));
-    }
-
 }
 
 #[cfg(test)]
@@ -170,8 +168,6 @@ mod tests {
     #[test]
     fn internal() {
         let mut first_gen: Vec<game_of_life::CellCoordinates> = Vec::new();
-        //let life_rule = setup::build_life_rule([false,false,false,true,false,false,true,false,false], [false,true,true,false,false,true,false,false,false]);
-        //let mut generation= game_of_life::create_next_generation_concurrent(&first_gen, &life_rule);
         create_random_generation_vector(&mut first_gen, 300);
         assert_eq!(300, first_gen.len());
     }
